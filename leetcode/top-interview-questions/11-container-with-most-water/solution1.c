2d6eaac51661f06c8e5162cf9c08893a43ee6236
unbeatedkb <1158309131@qq.com>
2020-07-22 11:54:45 +0800

    a

--- leetcode/top-interview-questions/11-container-with-most-water/solution1.c
+++ leetcode/top-interview-questions/11-container-with-most-water/solution1.c
@@ -9,6 +9,7 @@ int get_area(int a, int b, int length)
         height = a;
     printf("left is: %d, right is: %d\n", a, b);
     printf("length is: %d\n", height);
+    printf("area is: %d\n", length * height);
     return length * height;
 }
 
@@ -21,7 +22,11 @@ int max_area(int* height, int height_size)
     // 存放部分中间结果，这种情况下的中间结果并不多
     int max = get_area(height[left], height[right], right - left);
     
-    int scan_left = 1;
+    int scan_left;
+    if (height[left] < height[right])
+        scan_left = 1;
+    else
+        scan_left = 0;
     while (left < right) {
         if (left == right - 1) {
             int value = get_area(height[left], height[right], right - left);
@@ -32,21 +37,19 @@ int max_area(int* height, int height_size)
         }
 
         if (scan_left) {
-            if (height[left+1] > height[left]) {
-                int value = get_area(height[left+1], height[right], right - left - 1);
-                if (value > max)
-                    max = value;
-                scan_left = 0; // 切换遍历方向
-            }
             left ++;
+            int value = get_area(height[left], height[right], right - left);
+            if (value > max)
+                max = value;
+            if (height[left] > height[right])
+                scan_left = 0; // 切换遍历方向
         } else {
-            if (height[right-1] > height[right]) {
-                int value = get_area(height[left], height[right-1], right - 1 - left);
-                if (value > max)
-                    max = value;
-                scan_left = 1; // 切换遍历方向
-            }
             right --;
+            int value = get_area(height[left], height[right], right - left);
+            if (value > max)
+                max = value;
+            if (height[right] > height[left])
+                scan_left = 1; // 切换遍历方向
         }
     }
     return max;
@@ -55,7 +58,9 @@ int max_area(int* height, int height_size)
 int main()
 {
     // int array[] = {1,8,6,2,5,4,8,3,7};
-    int array[] = {2,3,10,5,7,8,9};
+    // int array[] = {2,3,10,5,7,8,9};
+    // int array[] = {10,9,8,7,6,5,4,3,2,1};
+    int array[] = {5,2,12,1,5,3,4,11,9,4};
     int max = max_area(array, sizeof(array)/4);
     printf("max is: %d\n", max);
 }
