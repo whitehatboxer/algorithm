bfef3a0553ed6782d61c5a108ce29ad24c305726
unbeatedkb <1158309131@qq.com>
2020-12-07 22:46:48 +0800

    sync

---
+++ bytedance/update-dict-according-to-layers/s.go
@@ -0,0 +1,15 @@
+package main
+
+// 使用 go 的话，感觉没法处理第一个参数，也没法使用递归
+
+func genNewMap(_map map[string]interface{}, layers []string, value map[string]string) {
+	if _map == nil {
+		return
+	}
+
+	if len(layers) == 0 {
+
+	}
+}
+
+func main() {}
